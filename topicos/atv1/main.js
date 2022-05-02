b97d727d0b380e9f9dfaf213e314247da03120e3
Marcus Vinicius Leite <marcus.leite.ext@rendimento.com.br>
2022-05-02 18:22:40 -0300

    feat: inputs error treatment

--- topicos/atv1/main.js
+++ topicos/atv1/main.js
@@ -84,7 +84,23 @@ const API_URL = 'https://app.professordaniloalves.com.br/api/v1';
 
 const form = document.querySelector('#form');
 
+const formFields = [
+  'nomeCompleto',
+  'email',
+  'dataNascimento',
+  'sexo',
+  'cpf',
+  'cep',
+  'numeroLogradouro',
+  'logradouro',
+  'cidade',
+  'uf',
+]
+
 async function submitData(){
+  
+  resetErrorsInput();
+
   const data = {
     nomeCompleto: form[0].value,
     email: form[1].value,
@@ -115,18 +131,79 @@ async function submitData(){
       }
       return Promise.reject(res)
     })
+
+    console.log(resp);
+    Swal.fire(`Sucesso!`, `${resp.message} `, 'success')
   
-    const content =  resp
-  
-    console.log('content',content );
   }catch(e){
+
     const errorResp = await e.json()
+
     console.log('error ', errorResp);
+
+    if(errorResp && errorResp.errors) setErrorsInput(errorResp.errors);
+
     Swal.fire(`Erro!`, `${errorResp.message} `, 'error')
+
   }
 
-  
-  
-  
 }
 
+
+function setErrorsInput(errors){
+
+  for (var [key, value] of Object.entries(errors)) {
+    console.log(key + ' ' + value); // "a 5", "b 7", "c 9"
+
+    let msgs = ''
+    value.forEach(msg => {
+      msgs += msg
+    });
+
+    const spanError = document.querySelector(`.${key} > span`);
+    spanError.classList.remove('hidden');
+    spanError.innerHTML = msgs;
+
+    const inputBorder = document.querySelector(`.${key} > input`)? document.querySelector(`.${key} > input`) : document.querySelector(`.${key} > select`)
+    inputBorder.classList.remove('border-cGreen')
+    inputBorder.classList.add('border-red-500')
+
+  }
+
+  /* if(errors.nomeCompleto){
+
+    let msgs = ''
+    errors.nomeCompleto.forEach(msg => {
+      msgs += msg
+    });
+
+    const spanError = document.querySelector('.nomeCompleto > span');
+    spanError.classList.remove('hidden');
+    spanError.innerHTML = msgs;
+
+    const inputBorder = document.querySelector('.nomeCompleto > input');
+    inputBorder.classList.remove('border-cGreen')
+    inputBorder.classList.add('border-red-500')
+
+  }
+  if(errors.email);
+  if(errors.dataNascimento);
+  if(errors.sexo);
+  if(errors.cpf);
+  if(errors.cep);
+  if(errors.numeroLogradouro);
+  if(errors.logradouro);
+  if(errors.cidade);
+  if(errors.uf); */
+}
+
+function resetErrorsInput(){
+ formFields.forEach(el => {
+  const spanError = document.querySelector(`.${el} > span`);
+  spanError.classList.add('hidden');
+
+  const inputBorder = document.querySelector(`.${el} > input`)? document.querySelector(`.${el} > input`) : document.querySelector(`.${el} > select`)
+  inputBorder.classList.add('border-cGreen')
+  inputBorder.classList.remove('border-red-500')
+ })
+}
